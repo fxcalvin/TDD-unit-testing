f843a81f09e5d9d1a194a7907ebbf39acc05a680
natasha-cloud <tindynatasha@gmail.com>
2023-10-16 21:25:31 +0300

    Added test for 20% tax calculation

--- test/testTaxCalculator.js
+++ test/testTaxCalculator.js
@@ -7,9 +7,9 @@ describe('Tax Calculator', function() {
     assert.equal(calculateTax(10000), 0);
   });
 
-  // it('should calculate 20% tax for earnings between 12,000 and 36,000', function() {
-  //   assert.equal(calculateTax(24000), 4800);
-  // });
+  it('should calculate 20% tax for earnings between 12,000 and 36,000', function() {
+    assert.equal(calculateTax(24000), 4800);
+  });
 
   // it('should calculate 40% tax for earnings greater than 36,000', function() {
   //   assert.equal(calculateTax(50000), 20000);
